d4ef09c934b8b10fd28e1b458d8534e86104ab7a
DavidRZuluaga <git@davidrzuluaga.com>
2018-12-03 17:31:43 -0500

    Updates

---
+++ precache-manifest.9111f349778dbd05e561ef52b2e5dff3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ec46125784e0b1f6cca6",
+    "url": "/react-app/static/js/runtime~main.ec461257.js"
+  },
+  {
+    "revision": "518c0d54cc0cb76b97a1",
+    "url": "/react-app/static/js/main.518c0d54.chunk.js"
+  },
+  {
+    "revision": "b547e1320b0d172e85e3",
+    "url": "/react-app/static/js/1.b547e132.chunk.js"
+  },
+  {
+    "revision": "518c0d54cc0cb76b97a1",
+    "url": "/react-app/static/css/main.a51c2ec1.chunk.css"
+  },
+  {
+    "revision": "72fdb11951c39c328f0214823b238583",
+    "url": "/react-app/index.html"
+  }
+];
